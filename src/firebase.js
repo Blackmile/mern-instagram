8fa9adb7ed091d0afd7f2b95f47556c1b7c2b1f0
Stanley Eze <stanley.ezeifeanyi@gmail.com>
2022-01-19 11:37:05 +0100

    install dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBucH7Kzm8Ds5CbuUs8uQJlzTPh3eZcTzA",
-  authDomain: "instagram-clone-23884.firebaseapp.com",
-  databaseURL: "https://instagram-clone-23884.firebaseio.com",
-  projectId: "instagram-clone-23884",
-  storageBucket: "instagram-clone-23884.appspot.com",
-  messagingSenderId: "671034896143",
-  appId: "1:671034896143:web:3aceafdf2319c9f1fc587a",
+  apiKey: "AIzaSyAOu0k_6UbL2HEb3jVlzFngzPe0EhU2ph4",
+  authDomain: "instagram-clone-a84e2.firebaseapp.com",
+  projectId: "instagram-clone-a84e2",
+  storageBucket: "instagram-clone-a84e2.appspot.com",
+  messagingSenderId: "494865521567",
+  appId: "1:494865521567:web:38e898030600ecc9f47baa",
+  measurementId: "G-HF1733DXKC"
 });
 
 const db = firebaseApp.firestore();
